c6cdd650b2097bb4de19478981bc6497ff17fea1
Thiago Nascimento <thiagosilvanascimento2015@gmail.com>
2023-05-31 11:29:43 -0300

    Criação e configuração dos componentes da tela de login

---
+++ src/app/pages/login/components/token/token.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-token-form',
+  templateUrl: './token.component.html',
+  styleUrls: ['./token.component.css'],
+})
+export class TokenComponent {}
