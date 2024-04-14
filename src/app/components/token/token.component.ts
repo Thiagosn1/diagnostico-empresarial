d073a5865dd7c0e1b9d382ded70fafbf47a56f66
Thiago Nascimento <70329966+Thiagosn1@users.noreply.github.com>
2024-04-13 21:41:17 -0300

    Alterações no componente token

--- src/app/components/token/token.component.ts
+++ src/app/components/token/token.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { EmailService } from '../../services/email.service';
 import { HttpResponse } from '@angular/common/http';
@@ -12,8 +18,9 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './token.component.html',
   styleUrls: ['./token.component.css'],
 })
-export class TokenComponent {
-  token = '';
+export class TokenComponent implements OnInit {
+  @ViewChild('input1') firstInput!: ElementRef;
+  token: string[] = Array(6).fill('');
   tokenError = '';
   tokenSuccess = '';
 
@@ -23,92 +30,103 @@ export class TokenComponent {
     private userService: UserService,
     private authService: AuthService,
     private businessesService: BusinessesService,
-    private answersService: AnswerService
+    private answersService: AnswerService,
+    private renderer: Renderer2
   ) {}
 
+  ngOnInit() {
+    if (!this.emailService.email) {
+      this.router.navigate(['/login']);
+    }
+  }
+
   enviarToken() {
-    if (this.token.length !== 6) {
+    if (this.token.some((t) => t.length !== 1)) {
       this.tokenError =
         'Código inválido, verifique se você digitou corretamente ou solicite um novo código.';
-    } else {
-      this.emailService.enviarToken(this.token).subscribe({
-        next: (response: HttpResponse<any>) => {
-          const authHeader = response.headers.get('Authorization');
-          if (authHeader !== null) {
-            this.authService.setToken(authHeader);
-            this.userService.obterUsuario().subscribe({
-              next: (usuario: any) => {
-                if (usuario.authority === 'ROOT') {
-                  this.router.navigate(['/admin/dashboard']);
-                } else if (usuario.authority === 'DEFAULT') {
-                  this.businessesService.obterEmpresas().subscribe({
-                    next: (empresas) => {
-                      if (empresas.length > 0) {
-                        this.router.navigate(['/gestao-empresa']);
-                      } else {
-                        this.answersService.buscarBusinessUserId().subscribe({
-                          next: (businessUserId) => {
-                            if (businessUserId) {
-                              this.router.navigate(['/info']);
-                            } else {
-                              this.router.navigate(['/cadastro']);
-                            }
-                          },
-                          error: (error) => {
-                            console.error(
-                              'Erro ao buscar o businessUserId:',
-                              error
-                            );
-                            this.router.navigate(['/cadastro']);
-                          },
-                        });
-                      }
-                    },
-                    error: (error) => {
-                      if (error.error.title === 'Negócio não encontrado') {
-                        this.answersService.buscarBusinessUserId().subscribe({
-                          next: (businessUserId) => {
-                            if (businessUserId) {
-                              this.router.navigate(['/info']);
-                            } else {
-                              this.router.navigate(['/cadastro']);
-                            }
-                          },
-                          error: (error) => {
-                            console.error(
-                              'Erro ao buscar o businessUserId:',
-                              error
-                            );
-                            this.router.navigate(['/cadastro']);
-                          },
-                        });
-                      }
-                    },
-                  });
-                }
-              },
-              error: (error) => {
-                console.error('Erro ao obter o usuário:', error);
-              },
-            });
-          }
-        },
-        error: (error) => {
-          console.error(error);
-          if (
-            error.error.title === 'Falha ao autenticar usuario' ||
-            error.error.title === 'Seu Código expirou' ||
-            error.error.title === 'Seu c�digo expirou'
-          ) {
-            this.tokenError =
-              'Código inválido, verifique se você digitou corretamente ou solicite um novo código.';
-            setTimeout(() => {
-              this.tokenError = '';
-            }, 2000);
-          }
-        },
-      });
+      return;
     }
+    const tokenStr = this.token.join('');
+    this.emailService.enviarToken(tokenStr).subscribe({
+      next: (response: HttpResponse<any>) => {
+        const authHeader = response.headers.get('Authorization');
+        if (authHeader !== null) {
+          this.authService.setToken(authHeader);
+          this.userService.obterUsuario().subscribe({
+            next: (usuario: any) => {
+              if (usuario.authority === 'ROOT') {
+                this.router.navigate(['/admin/dashboard']);
+              } else if (usuario.authority === 'DEFAULT') {
+                this.businessesService.obterEmpresas().subscribe({
+                  next: (empresas) => {
+                    if (empresas.length > 0) {
+                      this.router.navigate(['/gestao-empresa']);
+                    } else {
+                      this.answersService.buscarBusinessUserId().subscribe({
+                        next: (businessUserId) => {
+                          if (businessUserId) {
+                            this.router.navigate(['/info']);
+                          } else {
+                            this.router.navigate(['/cadastro']);
+                          }
+                        },
+                        error: (error) => {
+                          console.error(
+                            'Erro ao buscar o businessUserId:',
+                            error
+                          );
+                          this.router.navigate(['/cadastro']);
+                        },
+                      });
+                    }
+                  },
+                  error: (error) => {
+                    if (error.error.title === 'Negócio não encontrado') {
+                      this.answersService.buscarBusinessUserId().subscribe({
+                        next: (businessUserId) => {
+                          if (businessUserId) {
+                            this.router.navigate(['/info']);
+                          } else {
+                            this.router.navigate(['/cadastro']);
+                          }
+                        },
+                        error: (error) => {
+                          console.error(
+                            'Erro ao buscar o businessUserId:',
+                            error
+                          );
+                          this.router.navigate(['/cadastro']);
+                        },
+                      });
+                    }
+                  },
+                });
+              }
+            },
+            error: (error) => {
+              console.error('Erro ao obter o usuário:', error);
+            },
+          });
+        }
+      },
+      error: (error) => {
+        console.error(error);
+        if (
+          error.error.title === 'Falha ao autenticar usuario' ||
+          error.error.title === 'Seu Código expirou' ||
+          error.error.title === 'Seu c�digo expirou'
+        ) {
+          this.tokenError =
+            'Código inválido, verifique se você digitou corretamente ou solicite um novo código.';
+          setTimeout(() => {
+            this.tokenError = '';
+          }, 2000);
+          this.token = Array(6).fill('');
+          this.firstInput.nativeElement.focus();
+          return;
+        }
+      },
+    });
   }
 
   reenviarToken() {
@@ -127,5 +145,25 @@ export class TokenComponent {
 
   temEmail(): boolean {
     return !!this.emailService.email;
+  }
+
+  focarProximo(evento: KeyboardEvent, proximoInput?: HTMLInputElement) {
+    const input = evento.target as HTMLInputElement;
+    if (input.value.length === 1 && proximoInput) {
+      proximoInput.focus();
+    }
+    if (this.token.every((t) => t.length === 1)) {
+      this.enviarToken();
+    }
+  }
+
+  colarToken(event: ClipboardEvent) {
+    if (event.clipboardData) {
+      let pastedText = event.clipboardData.getData('text');
+      if (pastedText.length === 6) {
+        this.token = pastedText.split('');
+        event.preventDefault();
+      }
+    }
   }
 }
